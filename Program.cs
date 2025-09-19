f1bcde5d4f16f2904ef973adcb77b4c5a8e5e1ce
blobular1 <davis_o@bryanston.co.uk>
2025-09-19 08:12:28 +0000

    done

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@ class Program
     {
         // Here is your empty program!
         Console.Clear();
-        Console.WriteLine("Hello world!");
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+        string mod = "";
+        while (num > 0)
+        {
+            mod = (num % 2) + mod;
+            num /= 2;
+        }
+        Console.WriteLine(mod);
     }
 }
